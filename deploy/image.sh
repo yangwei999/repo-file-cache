1e99029c6a32073831f94d3221ec538c42d720e1
zengchen1024 <chenzeng2@huawei.com>
2021-11-02 11:20:42 +0800

    fix wrong image repository

--- deploy/image.sh
+++ deploy/image.sh
@@ -10,7 +10,10 @@ cd $(dirname $0)
 branch=$(git rev-parse --abbrev-ref HEAD)
 commit_id=$(git describe --tags --always --dirty)
 image_tag="${branch}-${commit_id}"
-repository=$(pwd | xargs dirname | xargs basename)
+repository=$(git remote -v | tail -1 | awk -F '/' '{print $NF}' | awk -F '.' '{print $1}')
+if [ -z "$repository" ]; then
+    repository=$(pwd | xargs dirname | xargs basename)
+fi
 
 image_registry=${IMAGE_REGISTRY_OVERRIDE:-swr.cn-north-4.myhuaweicloud.com}
 image_repo=${IMAGE_REPO_OVERRIDE:-opensourceway/robot/$repository}
@@ -21,6 +24,7 @@ IMAGE_REGISTRY ${image_registry}
 IMAGE_REPO ${image_repo}
 IMAGE_TAG ${image_tag}
 IMAGE_ID ${image_registry}/${image_repo}:${image_tag}
+CODE_REPOSITORY ${repository}
 EOF
 
 cd $work_dir
